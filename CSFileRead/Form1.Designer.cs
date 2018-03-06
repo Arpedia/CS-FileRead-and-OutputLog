659dbd9ad4ee31652bf8566dabd5833274943930
Arpedia <planes.next1@gmail.com>
2018-03-06 22:28:52 +0900

    initial commit

---
+++ CSFileRead/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CSFileRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FilePath = new System.Windows.Forms.TextBox();
+            this.FileOpenButton = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.LogBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FilePath
+            // 
+            this.FilePath.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FilePath.Location = new System.Drawing.Point(12, 12);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.ReadOnly = true;
+            this.FilePath.Size = new System.Drawing.Size(355, 29);
+            this.FilePath.TabIndex = 0;
+            this.FilePath.Text = "ファイルを開いてください";
+            // 
+            // FileOpenButton
+            // 
+            this.FileOpenButton.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FileOpenButton.Location = new System.Drawing.Point(373, 12);
+            this.FileOpenButton.Name = "FileOpenButton";
+            this.FileOpenButton.Size = new System.Drawing.Size(113, 29);
+            this.FileOpenButton.TabIndex = 1;
+            this.FileOpenButton.Text = "FileOpen";
+            this.FileOpenButton.UseVisualStyleBackColor = true;
+            this.FileOpenButton.Click += new System.EventHandler(this.FileOpenButton_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog_FileOk);
+            // 
+            // LogBox
+            // 
+            this.LogBox.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.LogBox.Location = new System.Drawing.Point(13, 48);
+            this.LogBox.Multiline = true;
+            this.LogBox.Name = "LogBox";
+            this.LogBox.ReadOnly = true;
+            this.LogBox.Size = new System.Drawing.Size(473, 453);
+            this.LogBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 513);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.FileOpenButton);
+            this.Controls.Add(this.FilePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FilePath;
+        private System.Windows.Forms.Button FileOpenButton;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.TextBox LogBox;
+    }
+}
+
---
+++ CSFileRead/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSFileRead
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FileOpenButton_Click(object sender, EventArgs e)
+        {
+            this.OpenFileDialog.ShowDialog();
+            this.FileOpenButton.Enabled = false;
+        }
+
+        private void OpenFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            this.FilePath.Text = this.OpenFileDialog.FileName;
+            this.FileOpenButton.Enabled = true;
+            this.WriteLog(this.FilePath.Text);
+        }
+
+        private void WriteLog(string path)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(path, Encoding.GetEncoding("Shift_JIS"));
+                String line = "";
+                int row = 1;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    this.WriteLine($"{row} :\t{line}");
+                    row += 1;
+                }
+            }
+            catch (Exception e)
+            {
+                this.WriteLine($"[ERROR]{ e.ToString() }");
+            }
+        }
+
+        private void WriteLine(string text)
+        {
+            this.LogBox.SelectionStart = this.LogBox.Text.Length;
+            this.LogBox.SelectionLength = 0;
+            this.LogBox.SelectedText = $"{text}\n";
+        }
+
+    }
+}
